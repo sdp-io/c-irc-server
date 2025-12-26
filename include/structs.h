4fdf9595ec64973024b3f1dc3e626fc371619766
sdp-io <samuelpetersonacs@gmail.com>
2025-12-25 22:07:48 -0500

    Add structs header with definitions for User and UserNode

---
+++ include/structs.h
@@ -0,0 +1,23 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+/*
+ * Struct containing information for a user currently on the IRC server.
+ * Allows for ease of nickname verification and messaging capabilities.
+ */
+struct User {
+  char *nick;
+  int user_fd;
+};
+
+/*
+ * Struct which acts as a container for the User struct.
+ * Allows for a linked list implementation to traverse, retrieve, and modify
+ * users that are currently active on the IRC server.
+ */
+struct UserNode {
+  struct User user_info;
+  struct User next;
+};
+
+#endif
